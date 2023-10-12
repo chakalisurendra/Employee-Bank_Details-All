bf9fed02b2da431ea10c6b049259c4c3b0d3b69d
surendra <surendra.chakali@hyniva.com>
2023-10-12 14:41:34 +0530

    get all

--- api.js
+++ api.js
@@ -228,6 +228,35 @@ const bankDetailsAll = async (event) => {
         });
       }
       break;
+      case `/employee/bankDetails/getAll`:
+        try {
+          const { Items } = await client.send(
+            new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME })
+          ); // Getting table name from the servetless.yml and setting to the TableName
+      
+          if (Items.length === 0) {
+            // If there is no employee bank details found
+            response.statusCode = 404; // Setting the status code to 404
+            response.body = JSON.stringify({
+              message: "Employee bank details not found.",
+            }); // Setting error message
+          } else {
+            // If employee bank details found in the dynamoDB setting the data
+            response.body = JSON.stringify({
+              message: "Successfully retrieved all Employees bank details.",
+              data: Items.map((item) => unmarshall(item)), // A DynamoDB record into a JavaScript object and setting to the data
+            });
+          }
+        } catch (e) {
+          // If any errors will occurred
+          console.error(e);
+          response.body = JSON.stringify({
+            statusCode: e.statusCode, // Handle any server response errors
+            message: "Failed to retrieved Employee bank details.",
+            errorMsg: e.message, // Handle any server response message
+          });
+        }
+        break;
   }
   return response;
 };

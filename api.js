078ab3160855b9e3c28948f8a4e97c6ef4539cb8
surendra <surendra.chakali@hyniva.com>
2023-10-12 14:17:04 +0530

    get added to the api

--- api.js
+++ api.js
@@ -191,6 +191,40 @@ const bankDetailsAll = async (event) => {
         }
       }
       break;
+    case `/employee/bankDetails/get/{empId}`:
+      try {
+        // Define table name and employeeId key with its value
+        const params = {
+          TableName: process.env.DYNAMODB_TABLE_NAME, // Getting table name from the servetless.yml and setting to the TableName
+          Key: marshall({ empId: event.pathParameters.empId }), // Convert a JavaScript object into a DynamoDB record.
+        };
+        //await response from db when sent getItem command with params
+        //containing tablename, key and only display empId and bank details
+        const { Item } = await client.send(new GetItemCommand(params)); //An asynchronous call to DynamoDB to retrieve an item
+        console.log({ Item });
+        if (!Item) {
+          // If there is no employee bank details found
+          response.statusCode = 404; // Setting the status code to 404
+          response.body = JSON.stringify({
+            message: "Employee bank details not found.",
+          }); // Setting error message
+        } else {
+          // If employee bank details found in the dynamoDB set to data
+          response.body = JSON.stringify({
+            message: "Successfully retrieved Employee bank details.",
+            data: unmarshall(Item), // A DynamoDB record into a JavaScript object and setting to the data
+          });
+        }
+      } catch (e) {
+        // If any errors will occurred
+        console.error(e);
+        response.body = JSON.stringify({
+          statusCode: e.statusCode, // Set server side status code
+          message: "Failed to retrieved employee bank details.",
+          errorMsg: e.message, // Set error message
+        });
+      }
+      break;
   }
   return response;
 };
